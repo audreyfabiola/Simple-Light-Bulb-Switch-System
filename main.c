6197091c4803b62c9c5f44c6fdd4907c3c870063
Clarissa Audrey Fabiola <114067267+audreyfabiola@users.noreply.github.com>
2023-01-02 23:24:50 +0700

    Update main.c

--- main.c
+++ main.c
@@ -4,28 +4,101 @@
 
 #include "mylib/mylib.h"
 
-void red() {
-    printf("\033[0;31m");
+void background_gray() {
+    printf("\033[100m");
 }
 
-void yellow() {
-    printf("\033[0;33m");
+void white_on_red() {
+    printf("\033[41;37m"); // White text on red background
+}
+
+void background_yellow() {
+    printf("\033[43m");
+}
+
+void white_on_green() {
+    printf("\033[37;42m"); // White text on green background
 }
 
 void reset() {
     printf("\033[0m");
 }
 
+void OFF() {
+    printf("    _____\n");
+    printf("   |__ __|\n");
+    printf("     |=|\n"); 
+    printf("     /");
+    background_gray();
+    printf(" ");
+    reset();
+    printf("\\\n");
+    printf("    (");
+    background_gray();
+    printf("   ");
+    reset();
+    printf(")\n");
+    printf("     `-'\n");
+    printf("                 _______________________\n");
+    printf("                |        _______        |\n");
+    printf("                |       ||");
+    white_on_red();
+    printf(" OFF ");
+    reset();
+    printf("||        |\n");
+    printf("                |       ||_____||       |\n");
+    printf("                |       |/    /||       |\n");
+    printf("                |       /    / ||       |\n");
+    printf("                |      /____/ /-'       |\n");
+    printf("                |      |____|/          |\n");
+    printf("                |                       |\n");
+    printf("                `-----------------------'\n");
+}
+
+    
+void ON() {
+    printf("    _____ \n");
+    printf("   |__ __| \n");
+    printf("     |=| \n");
+    printf("     /");  
+    background_yellow();
+    printf(" "); 
+    reset();
+    printf("\\ \n");
+    printf("--- (");
+    background_yellow();
+    printf("   ");
+    reset();
+    printf(") --- \n");
+    printf("   , `-'.\n");
+    printf("  /   |  \ \\\n");  
+    printf("                 _______________________ \n");
+    printf("                |                       | \n");
+    printf("                |       ____            | \n");
+    printf("                |      |____|\\          | \n");  
+    printf("                |      \\    \\ \\-'       | \n");
+    printf("                |       \\    \\ ||       | \n");
+    printf("                |       |\\____\\||       | \n");
+    printf("                |       ||");
+    white_on_green();
+    printf(" ON  ");
+    reset();
+    printf("||       | \n");
+    printf("                |       ||_____||       | \n");
+    printf("                |                       | \n");
+    printf("                `-----------------------' \n");
+}
+
 int main(void) {
     int s0 = 0, s1 = 0, b, o0 = 0;
     while (true) {
-        printf("Bulb: ");
+        printf("\033[2J"); // Clear screen 
+        printf("\033[1;1H"); // Move cursor to upper left
+        printf("Bulb: \n");
         if (o0 == 0) {
-            red();
-            printf("OFF\n");
+            OFF();
         } else {
-            yellow();
-            printf("ON\n");
+            ON();
         }
         reset();
 
